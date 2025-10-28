699d08f8123c245b2388a78347c00f987e210648
LeonBijker <0351849@student.rocvantwente.nl>
2025-10-28 09:33:09 +0100

    Add UI health display and update Health script
    Introduced a Canvas with a text element to display health in the scene. Updated the Health script to manage and display health changes via the UI, including methods for resetting, damaging, and healing health, and clamping values to a maximum. Also added SceneTemplateSettings.json to project settings.

--- TurnBasedGamePro/Assets/Scripts/Leon/Health.cs
+++ TurnBasedGamePro/Assets/Scripts/Leon/Health.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     [SerializeField]int health;
+    int maxHealth;
+    [SerializeField] Text healthText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,19 +15,46 @@ public class Health : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.B))
+        {
+            getHealth(100);
+            Debug.Log("Health reset to 100");
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            Damage(60);
+            Debug.Log("Took 60 damage");
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            Healing(health);
+            Debug.Log("Healed 25 health");
+        }
     }
     public int getHealth(int pHealth)
     {
-        health = pHealth;
+        health = Mathf.Clamp(pHealth,0,maxHealth);
+        settext();
         return health;
     }
+    public int MaxHealth(int pMaxHealth)
+    {
+        maxHealth = pMaxHealth;
+        return maxHealth;
+    }   
     public void Damage(int damage)
     {
         health -= damage;
+        settext();
     }   
     public void Healing(int pCurrentHealth)
     {
-        pCurrentHealth =+ health/100*20;
+        pCurrentHealth = pCurrentHealth + 25;
+        health = Mathf.Clamp(pCurrentHealth, 0, maxHealth);
+        settext();
+    }
+    void settext()
+    {
+        healthText.text = "Health: " + health.ToString();
     }
 }
