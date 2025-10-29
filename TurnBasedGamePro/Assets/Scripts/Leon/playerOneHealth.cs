6865f49f95f65e57d634e134cfff538184691705
LeonBijker <0351849@student.rocvantwente.nl>
2025-10-29 13:13:21 +0100

    Refactor scene and update player health scripts
    Replaced 'TestPlayer2' and 'Arm' GameObjects with new 'BlackTrees' and 'Grass' objects in the ShimonAttacksLeon scene, including tilemap changes. Removed associated components and scripts from the old objects. Updated playerOneHealth.cs and playerTwoHealth.cs, and made changes to CheckDamage.cs to reflect the new scene structure.

--- TurnBasedGamePro/Assets/Scripts/Leon/playerOneHealth.cs
+++ TurnBasedGamePro/Assets/Scripts/Leon/playerOneHealth.cs
@@ -2,22 +2,9 @@ using UnityEngine;
 
 public class PlayerOneHealth : Health
 {
-    protected override void Update()
+    public void Start()
     {
-        if (Input.GetKeyUp(KeyCode.B))
-        {
-            SetHealth(100);
-            Debug.Log("Player 1 health reset to 100");
-        }
-       /* else if (Input.GetKeyUp(KeyCode.A))
-        {
-            Damage(40);
-            Debug.Log("Player 1 took 40 damage");
-        }*/
-        else if (Input.GetKeyUp(KeyCode.D))
-        {
-            Heal(25);
-            Debug.Log("Player 1 healed 25");
-        }
+        SetHealth(100);
     }
+    
 }
