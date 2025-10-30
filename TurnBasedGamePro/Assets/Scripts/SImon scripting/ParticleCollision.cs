85f157f97c889fde6cfce3ad808cae9031b4f23b
Daniël Freriksen <0354092@student.rocvantwente.nl>
2025-10-30 14:20:03 +0100

    Add teleportation system and related assets
    Introduced teleportation input actions, handler scripts, and prefabs including TeleportMarker and TestPlayer. Updated scene organization, added new materials, and adjusted visual and lighting settings to support new gameplay features.

--- TurnBasedGamePro/Assets/Scripts/SImon scripting/ParticleCollision.cs
+++ TurnBasedGamePro/Assets/Scripts/SImon scripting/ParticleCollision.cs
@@ -2,13 +2,23 @@ using UnityEngine;
 
 public class ParticleCollision : MonoBehaviour
 {
-    [Header("Force Settings")]
-    [SerializeField] private float force = 5f;
+    [Header("Force Settings")] [SerializeField]
+    private float force = 5f;
+
     [SerializeField] private ForceMode2D forceMode = ForceMode2D.Impulse;
+
+    private PlayerController playerController;
 
     private void OnParticleCollision(GameObject other)
     {
+        playerController = other.GetComponent<PlayerController>();
+        if (playerController != null)
+        {
+            playerController.OnHit(2);
+        }
+
         Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+
         if (rb)
         {
             Vector2 dir = (rb.position - (Vector2)transform.position).normalized;
